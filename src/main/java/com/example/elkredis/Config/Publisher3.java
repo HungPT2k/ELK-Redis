ba8b8ca08dd8db7a2a16f745ac60114c5d90697c
HungPT2k <phanduyanh1512@gmail.com>
2023-09-11 09:22:49 +0700

    add publisher3

---
+++ src/main/java/com/example/elkredis/Config/Publisher3.java
@@ -0,0 +1,38 @@
+package com.example.elkredis.Config;
+
+import com.example.elkredis.model.MessageDTO1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Publisher3 {
+    @Autowired
+    private RedisTemplate<String, MessageDTO1> redisTemplate;
+    @Qualifier("topicA")
+    @Autowired
+    private ChannelTopic topicA;
+
+    @Qualifier("topicB")
+    @Autowired
+    private ChannelTopic topicB;
+
+
+    public void publishToA(MessageDTO1 messageDTO) {
+
+        redisTemplate.convertAndSend(topicA.getTopic(), messageDTO);
+        System.out.println("Sending request  to A: " );
+    }
+
+
+    public void addGuiRedis( MessageDTO1 messageDTO1) {
+
+        redisTemplate.opsForList().leftPush(messageDTO1.getMethod(),messageDTO1);
+        System.out.println("add request to redis gui: ");
+    }
+
+
+}
